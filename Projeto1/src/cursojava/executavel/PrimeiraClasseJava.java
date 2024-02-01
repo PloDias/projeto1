b4e27a57acdb52fb726fdc60adb58334b7ce6261
paulo.dias <paulo.dias@ADM_TI_03.agricer.local>
2024-02-01 14:07:20 -0300

    Criação do Projeto

---
+++ Projeto1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Meu Primeiro Java Program - Vamos!");
+		System.out.println("Meu Segundo Java Program - Vamos!");
+	}
+		
+
+}
